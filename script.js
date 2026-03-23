b84e2b411e5de0607390c74aa2ac98a91d85f883
Rena Katz <rena.d.katz@gmail.com>
2026-03-23 17:51:03 -0400

    adding functions

--- script.js
+++ script.js
@@ -10,12 +10,52 @@ const flashcards = [
 let currentIndex = 0;
 let showingTerm = true;
 
+card = document.getElementById("flashcard");
+next = document.getElementById("next-btn");
+previous = document.getElementById("previous-btn");
+addCard = document.getElementById("add-btn");
+
 // Start with this function to simply display the card
 function displayCard() {
+    if(showingTerm){
+        document.getElementById("card-content").innerText = flashcards[currentIndex].term;
+    } else{
+        document.getElementById("card-content").innerText = flashcards[currentIndex].definition;
+    }
+}
+
+function flipCard() {
+    if(showingTerm){
+        showingTerm = false;
+    } else {
+        showingTerm = true;
+    }
+    displayCard();
+}
+
+function nextCard() {
+    showingTerm = true;
+    currentIndex++;
+    if (currentIndex >= flashcards.length) {
+        currentIndex = 0;
+    }
+    displayCard();
 
 }
 
-// The rest of the code you will write is apart of event listeners
+function previousCard() {
+    showingTerm = true;
+    currentIndex--;
+    if(currentIndex < 0){
+        currentIndex = flashcards.length - 1;
+    }
+    displayCard();
+}
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
+
+// adding event listeners to the buttons
+card.addEventListener("click", flipCard);
+next.addEventListener("click", nextCard);
+previous.addEventListener("click", previousCard);
